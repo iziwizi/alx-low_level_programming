d8932e62942ff76c59ee3555dc7889a010921ba2
iziwizi <iziwizi090@gmail.com>
2022-07-12 00:03:18 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,15 +2,10 @@
 
 /**
  * _strcpy - function with two arguments
- *
  * @dest: 1st argument char type pointer
- *
  * @src: 2nd argument char type pointer
- *
  * Description: copies the string to an array
- *
  * Return: na
- *
 */
 
 char *_strcpy(char *dest, char *src)
